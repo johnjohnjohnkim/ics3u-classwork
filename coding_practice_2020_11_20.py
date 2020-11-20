00af0047e1e7d4ac6bf9ca2352666f3efafeed82
johnjohnjohnkim <74620172+johnjohnjohnkim@users.noreply.github.com>
2020-11-20 13:29:35 -0500

    Update coding_practice_2020_11_20.py

--- coding_practice_2020_11_20.py
+++ coding_practice_2020_11_20.py
@@ -21,3 +21,15 @@ height = int(input("Height: "))
 volume = length * width * height
 
 print(f"\nYour volume of the rectangle prism is: {volume} cm^3")
+
+4. Google meet if elif
+
+
+print("Do you join a Google meet and mute the teacher?: ")
+d1a = input("[A] Yes [B] No  ")
+
+if d1a == "A":
+    print("That's not good what are you doing with your life")
+
+elif d1a == "B":
+    print("Ok, good job, you're making your parents proud")
