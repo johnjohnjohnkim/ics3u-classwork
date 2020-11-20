4b9b46aa2a53afd20b03b0896b643a1465dc9639
johnjohnjohnkim <74620172+johnjohnjohnkim@users.noreply.github.com>
2020-11-20 13:13:15 -0500

    Update coding_practice_2020_11_20.py

--- coding_practice_2020_11_20.py
+++ coding_practice_2020_11_20.py
@@ -1,2 +1,12 @@
+1. Favourite Colour
+
 colour = (input("What is your favourite colour?: "))
 print(f"No way!! {colour} is actually the best colour ever created who ever doesn't  like {colour} should burn in hell :D")
+
+2. Total number of cans
+
+caninpack = int(input("How many cans are there in a pack?: "))
+numberpack = int(input("How many packs do you have?: "))
+totalcan = caninpack * numberpack
+
+print(f"The total number of cans are: {totalcan}")
