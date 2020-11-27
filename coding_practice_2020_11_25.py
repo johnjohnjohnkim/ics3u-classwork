c9eedae97f81108cfbf1d55aae5309fa388dcf37
johnjohnjohnkim <74620172+johnjohnjohnkim@users.noreply.github.com>
2020-11-26 21:24:54 -0500

    Update coding_practice_2020_11_25.py

--- coding_practice_2020_11_25.py
+++ coding_practice_2020_11_25.py
@@ -212,3 +212,42 @@ elif month == "December":
         season = "Winter"
 
 print(month, day, "is in", season)
+
+
+
+
+
+Coding Bat exercises 1-10
+
+1. 
+def hello_name(name): 
+  return "Hello " + name + "!"
+2.
+def make_abba(a, b):
+  return a+2*b+a
+  
+3.
+def make_tags(tag, word):
+  return "<" + tag + ">" + word + "</" +tag + ">"
+4.
+def make_out_word(out, word):
+  return out[:2] + word + out[2:]
+5. 
+def extra_end(str):
+  return str[-2:]+str[-2:]+str[-2:] 
+  return str[-2:]*3
+6.
+def first_two(str):
+  return str[:2]
+7.
+def first_half(str):
+  return str[:len(str)/2]
+8.
+def without_end(str):
+  return str[1:-1]
+9.
+def combo_string(a, b):
+  return a+b+a if len(a)<len(b) else b+a+b
+10.
+def non_start(a, b):
+  return a[1:]+b[1:]
